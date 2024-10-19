c387cc840b13cb397c81cfb2a0b7786c200fcf96
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2024-10-19 15:37:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import cv2 as cv
 
+from utils.feature_matching import extract_sift_feature, visualize_sift_feature
+
 
 def get_config():
     parser = argparse.ArgumentParser()
@@ -18,3 +20,9 @@ args = get_config()
 image1 = cv.imread(args.image1_path)
 image2 = cv.imread(args.image2_path)
 
+# 이미지의 keypoints와 descriptors 추출하기
+keypoints1, descriptors1 = extract_sift_feature(image1)
+keypoints2, descriptors2 = extract_sift_feature(image2)
+
+visualize_sift_feature(image1, keypoints1, 'image1')
+visualize_sift_feature(image2, keypoints2, 'image2')
